f60d2ed410f908ec4f7c7d49314438fbc4a4a094
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-12-01 11:37:42 +0530

    Navigation and Header Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Fragment } from 'react'
 import './App.css'
+import Nav from './Components/Nav'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <Fragment>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </Fragment>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className='header container-fluid'>
+      <div className='row'>
+        <div className='d-flex align-items-center col-12 col-md-6 order-2 order-md-1'>
+          <div className='card bg-transparent border-0 p-2'>
+            <h1 className='hero-heading'>Little Lemon</h1>
+            <h3 className='hero-subheading'>Chicago</h3>
+            <p className='hero-caption'>
+              We are a family owned Mediterranean restaurant, focused on 
+              traditional recipes served with a modern twist.
+            </p>
+            <div>
+              <button className='btn btn-warning'>Reserve Table</button>
+            </div>
+          </div>
+        </div>
+        <div className='col-12 col-md-6 order-1 order-md-2'>
+          <div className='card bg-transparent border-0 p-2'>
+            <img className='rounded' src='https://images.pexels.com/photos/6287486/pexels-photo-6287486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='hero-image' />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
